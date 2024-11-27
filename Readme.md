b34e971b7dd6a8f44b63db81e33c483b26bc43a0
beejayofgod <adexbolaji100@gmail.com>
2024-11-27 17:25:01 +0100

    first commit looking forward to completing slider

---
+++ Readme.md
@@ -0,0 +1,1 @@
+a fintech landing page
---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      backgroundColor: {
+        hero_green: "#2B892E",
+        grey_bg: "#989898",
+        article_dark_grey: "#131313",
+        secondary_color: "#48D64C", // green
+      },
+      colors: {
+        light_grey: "#BDBDBD",
+        green_intro: "#2B892E",
+        secondary_color: "#48D64C", // light green
+      },
+      backgroundImage: {
+        hero: "url('./images/hero_mask.svg')",
+        hero_mask_desktop: " url('./images/hero_mask_desktop.png')",
+        "article-mask": "url('./images/article_mask.svg')",
+        financial_plan_mask_dsktop:
+          "url('./images/financial_plan_mask_desktop.svg')",
+        seamless_mask: "url('./images/seamless_mask_desktop.svg')",
+      },
+      backgroundSize: {
+        mask: "100% 30%",
+      },
+      objectPosition: {
+        happy: "50px -50px",
+      },
+    },
+  },
+  plugins: [],
+};
