2fa4201c251cff1d35605b59d043116bb09cab13
beejayofgod <adexbolaji100@gmail.com>
2024-11-28 08:22:42 +0100

    changed all alt text

--- main.js
+++ main.js
@@ -1,24 +1,45 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+let testimonial_btns = document.querySelector("#testimonial_btns");
+let testimonails_container = document.querySelector("#testimonials");
+let testimonail_slides = document.querySelectorAll(".testimonial_slide");
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+let index = 0;
+let no_of_moves;
+function moveToSlideIndex() {
+  testimonail_slides.forEach((slide) => {
+    slide.style.transform = window.matchMedia("(max-width: 740px)").matches
+      ? `translateX(-${index * 100}%)`
+      : `translateX(-${index * 300}%)`;
+  });
+}
 
-setupCounter(document.querySelector('#counter'))
+no_of_moves = window.matchMedia("(max-width: 740px)").matches
+  ? testimonail_slides.length - 1
+  : testimonail_slides.length / 3 - 1;
+
+window.addEventListener("resize", () => {
+  no_of_moves = window.matchMedia("(max-width: 740px)").matches
+    ? testimonail_slides.length - 1
+    : testimonail_slides.length / 3 - 1;
+});
+
+alert(testimonail_slides.length);
+
+testimonial_btns.addEventListener("click", (e) => {
+  if (e.target.classList.contains("testimonial_right")) {
+    if (index < no_of_moves) {
+      index += 1;
+      moveToSlideIndex();
+    } else if (index == no_of_moves) {
+      index = 0;
+      moveToSlideIndex();
+    }
+  } else {
+    if (index == 0) {
+      index = no_of_moves;
+      moveToSlideIndex();
+    } else {
+      index -= 1;
+      moveToSlideIndex();
+    }
+  }
+});
