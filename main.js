9c2e8f8d4b2f434072a34d51a4101ebd1d5c1379
beejayofgod <adexbolaji100@gmail.com>
2024-11-28 09:52:17 +0100

    implemented faq section

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
 let testimonial_btns = document.querySelector("#testimonial_btns");
 let testimonails_container = document.querySelector("#testimonials");
 let testimonail_slides = document.querySelectorAll(".testimonial_slide");
+let faq_arrows = Array.from(document.querySelectorAll(".faq_arrow"));
+let faq_answers = Array.from(document.querySelectorAll(".faq_ans"));
+
+faq_arrows.forEach((arrow) => {
+  arrow.addEventListener("click", () => {
+    let arrow_index = faq_arrows.indexOf(arrow);
+    let current_faq_answer = faq_answers[arrow_index];
+    let current_faq_arrow = faq_arrows[arrow_index];
+    let faq_questionisHidden = current_faq_answer.classList.contains("hidden");
+
+    if (faq_questionisHidden) {
+      current_faq_answer.classList.replace(`hidden`, `flex`);
+      current_faq_arrow.classList.add("rotate-180");
+      // faq_answer.classList.add("")
+    } else {
+      current_faq_answer.classList.replace(`flex`, `hidden`);
+      current_faq_arrow.classList.replace("rotate-180", "rotate-0");
+    }
+  });
+});
 
 let index = 0;
 let no_of_moves;
@@ -21,8 +41,6 @@ window.addEventListener("resize", () => {
     ? testimonail_slides.length - 1
     : testimonail_slides.length / 3 - 1;
 });
-
-alert(testimonail_slides.length);
 
 testimonial_btns.addEventListener("click", (e) => {
   if (e.target.classList.contains("testimonial_right")) {
